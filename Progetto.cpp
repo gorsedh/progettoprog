49428d1d8f7fd8bee9a77c0421e6f72df50a3c0c
gorsedh <69189728+gorsedh@users.noreply.github.com>
2020-08-20 13:18:28 +0200

    Update Progetto.cpp

--- Progetto.cpp
+++ Progetto.cpp
@@ -217,26 +217,26 @@ auto linearSpread(Population& previous) { //includere uno spread in cui i valori
     std::mt19937 gen(seed());
     std::uniform_real_distribution<> dis(0, 1);
 
-    for (int row = 1; row < size - 1; ++row) {
-        for (int column = 1; column < size - 1; ++column) {
+    for (int row_loop = 1; row_loop < size - 1; ++row_loop) {
+        for (int column_loop = 1; column_loop < size - 1; ++column_loop) {
 
-            assert(row != 0 && row != size && column != 0 && column != size);
+            assert(row_loop != 0 && row_loop != size && column_loop != 0 && column_loop != size);
 
-            auto cell = previous(row, column);
+            auto cell = previous(row_loop, column_loop);
             switch (cell) {
             case Condition::S: {
                 int i = 0;
 
-                while (i != adjacentInfects(previous, row, column)) {
+                while (i != adjacentInfects(previous, row_loop, column_loop)) {
                     ++i;
 
                 }
-                if (i == adjacentInfects(previous, row, column) &&
+                if (i == adjacentInfects(previous, row_loop, column_loop) &&
                     dis(gen) <= i * beta) { //modello di spread lineare fra (0,0) e (1,beta) 
 
-                    //dis(gen) <= booleanMarker(i) * ( (i-1)*(1-beta)/7 + beta) ) { //linear spread fra (1,beta) e (8,1) con annullamento sullo zero
+                   // dis(gen) <= booleanMarker(i) * ( (i-1)*(1-beta)/7 + beta) ) { //linear spread fra (1,beta) e (8,1) con annullamento sullo zero
 
-                    evolved(row, column) = (Condition::I);
+                    evolved(row_loop, column_loop) = (Condition::I);
                 }
                 else {
                 }
@@ -245,23 +245,23 @@ auto linearSpread(Population& previous) { //includere uno spread in cui i valori
             case Condition::I: {
                 if (dis(gen) <= gamma_) {
                     if (dis(gen) <= std::pow(gamma_, 2)) {
-                        evolved(row, column) = (Condition::D);
+                        evolved(row_loop, column_loop) = (Condition::D);
                     }
                     else {
-                        evolved(row, column) = (Condition::R);
+                        evolved(row_loop, column_loop) = (Condition::R);
                     }
                 }
                 else {
-                    evolved(row, column) = previous(row, column);
+                    evolved(row_loop, column_loop) = previous(row_loop, column_loop);
                 }
                 break;
             }
             case Condition::R: {
-                evolved(row, column) = previous(row, column);
+                evolved(row_loop, column_loop) = previous(row_loop, column_loop);
                 break;
             }
             case Condition::D: {
-                evolved(row, column) = previous(row, column);
+                evolved(row_loop, column_loop) = previous(row_loop, column_loop);
                 break;
             }
 
@@ -287,21 +287,21 @@ auto nonLinearSpread(Population& previous, int daysPassed) {
     if (daysPassed > 60 && daysPassed <= 100) { gamma_ = gamma_ * 1.5; } //dopo 60 giorni dallo scoppio dell'epidemia le tecniche di cura diventano più efficaci
     if (daysPassed > 101) { gamma_ = gamma_ * 2; } //dopo 100 giorni dallo scoppio dell'epidemia le tecniche di cura diventano ancora più efficaci
 
-    for (int row = 1; row < size - 1; ++row) {
-        for (int column = 1; column < size - 1; ++column) {
+    for (int row_loop = 1; row_loop < size - 1; ++row_loop) {
+        for (int column_loop = 1; column_loop < size - 1; ++column_loop) {
 
-            assert(row != 0 && row != size && column != 0 && column != size);
+            assert(row_loop != 0 && row_loop != size && column_loop != 0 && column_loop != size);
 
-            auto cell = previous(row, column);
+            auto cell = previous(row_loop, column_loop);
             switch (cell) {
             case Condition::S: {
                 int i = 0;
-                while (i != adjacentInfects(previous, row, column)) {
+                while (i != adjacentInfects(previous, row_loop, column_loop)) {
                     ++i;
                 }
-                if (i == adjacentInfects(previous, row, column) &&
+                if (i == adjacentInfects(previous, row_loop, column_loop) &&
                     dis(gen) <= ((beta * i) / (((size * size + previousDayInfects) / (size * size))))) { //riduce infettività all'aumentare degli infetti
-                    evolved(row, column) = (Condition::I);
+                    evolved(row_loop, column_loop) = (Condition::I);
                 }
                 else {
                 }
@@ -310,23 +310,23 @@ auto nonLinearSpread(Population& previous, int daysPassed) {
             case Condition::I: {
                 if (dis(gen) <= gamma_) {
                     if (dis(gen) <= std::pow(gamma_, 2)) {
-                        evolved(row, column) = (Condition::D);
+                        evolved(row_loop, column_loop) = (Condition::D);
                     }
                     else {
-                        evolved(row, column) = (Condition::R);
+                        evolved(row_loop, column_loop) = (Condition::R);
                     }
                 }
                 else {
-                    evolved(row, column) = previous(row, column);
+                    evolved(row_loop, column_loop) = previous(row_loop, column_loop);
                 }
                 break;
             }
             case Condition::R: {
-                evolved(row, column) = previous(row, column);
+                evolved(row_loop, column_loop) = previous(row_loop, column_loop);
                 break;
             }
             case Condition::D: {
-                evolved(row, column) = previous(row, column);
+                evolved(row_loop, column_loop) = previous(row_loop, column_loop);
                 break;
             }
 
@@ -341,19 +341,20 @@ auto nonLinearSpread(Population& previous, int daysPassed) {
 auto cellMover(Population& pop) {
 
     int iX, jX, iY, jY;
+    int redSize = static_cast<int> (pop.getSize()) - 2;
+    int numOfSwaps = pop.getSize(); 
+    for (int i = 0; i < numOfSwaps; i++) {
 
-    for (int i = 0; i < pop.getSize(); i++) {
-
-        iX = (rand() + time(nullptr)) % (pop.getSize() - 2) + 1;
-        jX = (rand() + time(nullptr)) % (pop.getSize() - 2) + 1;
-        iY = (rand() + time(nullptr)) % (pop.getSize() - 2) + 1;
-        jY = (rand() + time(nullptr)) % (pop.getSize() - 2) + 1;
+        iX = (rand() + time(nullptr)) % (redSize) + 1;
+        jX = (rand() + time(nullptr)) % (redSize) + 1;
+        iY = (rand() + time(nullptr)) % (redSize) + 1;
+        jY = (rand() + time(nullptr)) % (redSize) + 1;
 
         if (pop(iX, jX) != Condition::D && pop(iY, jY) != Condition::D) {
             pop.swapTwoCells(iX, jX, iY, jY);
         }
     }
-    return pop; 
+    return pop;
 }
 
 //initializations
@@ -443,7 +444,7 @@ int initSizeNoGraph() {
 double initBeta() {
     double betaval;
     std::cout << "\nEnter value for \033[31mbeta\033[0m (0 < x < 1), otherwise it will be set as default\n";
-    std::cout << "Suggested values are between 0.2 and 0.4, higher values will result in faster spreading\n";
+    std::cout << "Suggested values are between 0.1 and 0.2, higher values will result in faster spreading\n";
     std::cin >> betaval;
     if (betaval > 0 && betaval < 1) {
         beta = betaval;
@@ -547,7 +548,7 @@ auto noGraphicsExecute(ParametersCheck check, int size) {
     //far andare la funzione autonomamente
     while (pop.infectsCounter() != 0) {
         std::this_thread::sleep_for(std::chrono::milliseconds(50));
-        pop = linearSpread(pop);
+        //pop = linearSpread(pop);
         std::cout << "Day: " << dayspassed << "\n";
 
         dataPrint(pop);
@@ -572,6 +573,7 @@ auto bigSimulationExecute(ParametersCheck check, int size) {
     emptyBoard(pop);
     initializeInfect(pop);
     while (pop.infectsCounter() != 0) {
+        pop = cellMover(pop);
         pop = linearSpread(pop);
         std::cout << "\n";
         dataCollecting(pop, finalReport);
@@ -593,6 +595,7 @@ auto bigSimulationExecuteNL(ParametersCheck check, int size) {
     initializeInfect(pop);
     int daysPassed = 1;
     while (pop.infectsCounter() != 0) {
+        //pop = cellMover(pop);
         pop = nonLinearSpread(pop, daysPassed);
         std::cout << "\n";
         dataCollecting(pop, finalReport);
@@ -625,7 +628,7 @@ void printDataToFile(std::vector<dailyReport>& finalReport) {
 
 int main()
 {
-    std::cout << "Which optionwould you like to use?\n(\033[31ma\033[0m for auto settings, \033[31md\033[0m for disabled graphics, \033[31mb\033[0m for disabled graphics and output,\n\033[31mn\033[0m for a non linear spread and no graphic output, \033[31many other key\033[0m for manual settings)\n";
+    std::cout << "Which optionwould you like to use?\n(\033[31ma\033[0m for auto settings, \033[31md\033[0m for disabled graphics, \033[31mb\033[0m for disabled graphics and output,\n\033[31mn\033[0m for a non linear spread and no graphic output, \033[31mm\033[0m for manual settings)\n";
     std::cout << "a and manual are suggested for small populations to get a graphical output of the epidemic (size<30)\n";
     std::cout << "d is suggested for big populations (size>30)\n";
     std::cout << "b is suggested for very big populations just to have the report to analyze (size>100)\n";
@@ -663,7 +666,13 @@ int main()
         finalReport = bigSimulationExecuteNL(checkParNoOutput, boardSize);
     }
     break;
-
+    case 'm':
+    {
+        int const boardSize = initSize();
+        ParametersCheck const checkDefault = initializeparameters();
+        finalReport = execute(checkDefault, boardSize);
+    }
+    break;
     default:
     {
         int const boardSize = initSize();
@@ -681,6 +690,5 @@ int main()
         printDataToFile(finalReport);
         std::cout << "You will find the report in the directory of this file, remember to cancel it before running the program again!\n";
     }
-
 
 }
