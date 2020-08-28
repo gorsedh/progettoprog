a51c4a224d8fadbccc6cd5c240ec6b96dabc85e4
gorsedh <69189728+gorsedh@users.noreply.github.com>
2020-08-28 21:35:29 +0200

    Update structures.cpp

--- structures.cpp
+++ structures.cpp
@@ -1,9 +1,6 @@
-//#ifndef SIR_STRUCTURES_CPP
-//#define SIR_STRUCTURES_CPP
+/*This file provides the definition of the  global variables*/
 
 #include "structures.hpp"
 
 double beta = 0;
 double gamma_ = 0;
-
-//#endif
